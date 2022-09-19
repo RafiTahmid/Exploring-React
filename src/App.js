23fa93ad07186a0a22018d73c7a6828b985ac6b5
RafiTahmid <rafi.tahmid019@gmail.com>
2022-09-19 23:16:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 789;
+const singer = {name: 'Dr. Mahfuzur Rahman', job: 'Singer'}
+const singer2 = {name: 'Eva Rahman', job: 'Singer'}
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white'
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>HHeeeyy React</h3>
+        </div>
+        <div className="music">
+          <p>ID: { number }</p>
+          <p style={{color: 'blue', backgroundColor: 'yellow'}}>Name: { singer.name }</p>
+          <p style = {singerStyle}>Name: { singer2.name }</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
